f2c6920db7ec2c5122b00212cc8c7be4be352b6b
Aziola <gluskov934@gmail.com>
2026-02-25 14:02:27 +0300

    Создал 3 страницы для примеров

---
+++ Pages/Problem1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pr4_523_Glushkov_Sidorov.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Problem1.xaml
+    /// </summary>
+    public partial class Problem1 : Page
+    {
+        public Problem1()
+        {
+            InitializeComponent();
+        }
+    }
+}
