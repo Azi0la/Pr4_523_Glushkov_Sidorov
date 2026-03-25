a7a8ea90834a0d52dc26e58ee132d0e1a86b337c
Aziola <gluskov934@gmail.com>
2026-03-25 14:09:54 +0300

    Сделана половина тетового модуля 2-проблемы

--- UnitTestProject/UnitTest1.cs
+++ UnitTestProject/UnitTest1.cs
@@ -39,6 +39,48 @@ namespace UnitTestProject
         {
             var problem3 = new Problem3();
 
+            int res = 2 + 2;
+            Assert.AreEqual(res, 4);
+            Assert.AreNotEqual(res, 5);
+            Assert.IsFalse(res > 5);
+            Assert.IsTrue(res < 5);
+        }
+
+        [TestMethod]
+        public void TestMethodProblem2()
+        {
+            var problem2 = new Problem2();
+            double x = 2;
+            double y = 4;
+            string function = "sh(x)";
+            double fx;
+            switch (function)
+            {
+                case "sh(x)":
+                    fx = Math.Sinh(x);
+                    break;
+                case "x^2":
+                    fx = Math.Pow(x, 2);
+                    break;
+                case "e^x":
+                    fx = Math.Exp(x);
+                    break;
+                default:
+                    fx = Math.Sinh(x);
+                    break;
+            }
+            double test_res;
+            if (x > y)
+                test_res = Math.Pow(fx - y, 3) + Math.Atan(fx);
+            else if (x < y)
+                test_res = Math.Pow(y - fx, 3) + Math.Atan(fx);
+            else
+                test_res = Math.Pow(y + fx, 3) + 0.5;
+
+            Assert.AreEqual(test_res, 1.35371373880349);
+            Assert.AreNotEqual(test_res, 1.36);
+            Assert.IsFalse(test_res > 1.36);
+            Assert.IsTrue(test_res < 1.36);
         }
     }
 }
